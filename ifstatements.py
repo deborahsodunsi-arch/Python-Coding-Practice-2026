f0f9dd7a2468a7c9a1b9474e10c5770e39e7375d
Deborah <Deborahsodunsi@gmail.com>
2026-04-28 23:51:44 -0400

    5-1

---
+++ ifstatements.py
@@ -0,0 +1,41 @@
+# if statements practice
+
+simi_account = 1000000
+
+# simi bought a car 
+
+simi_account -= 30000
+loan = 30000
+
+if (simi_account == 1000000) :
+    # Buy house
+    print('Simi has a 1M house')
+elif ( 9800000 < simi_account < 1000000 ):
+    print("Simi buys a smaller house for 990000")
+elif (simi_account == 970,000) and (loan == 30000):
+    print("I will just mortgage a house for 1mil ")
+else:
+    print("I don't have enough for the $1M house ")
+
+# Basic if statement
+
+# if else statement
+
+# elif statement 
+
+
+# states_of_brazil = ["Sau Paulo", "Bahia", "Rio", "Maranhão"]
+# state = "Acre"
+
+# if state not in states_of_brazil:
+#     print(state.title() + " , Is another state that needs to be added to states of Brazil.")
+
+
+
+
+# cars = ["nissan", "chevy", "toyota", "bmw"]
+# car = "mercedes"
+
+# if car not in cars:
+#     print(f"i would love to have a {} added to this list")
+    
